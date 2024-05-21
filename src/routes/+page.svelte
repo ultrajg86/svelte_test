f8774b450faa40222973d8a23ee5cdf0fb2375af
ultrajg86 <ultrajg86@gmail.com>
2024-05-21 01:08:25 +0000

    svelte kit init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<p>열심히 해보자!!</p>
+
+<h1 class="text-3xl font-bold underline">Hello</h1>
